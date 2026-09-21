890ac3d27162fb33a4836b1411566593de47905b
Charles Dauda <charlesdauda676@gmail.com>
2026-09-21 23:05:48 +0000

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const NavBar = () => {
     ]
 
     return (
-        <header className="relative flex items-center px-4 py-6 m-4 h-16 ">
+        <header className="relative flex items-center px-4 py-6 m-4 h-16 w-full">
             <div className='h-8 w-20 ml-15'>
                 <img 
                 src={navlogo}
@@ -25,11 +25,17 @@ const NavBar = () => {
                  <ul className="flex space-x-4">
                 {navList.map(({name, link}) =>(
                     <li key={link}>
-                        <a href={link}>{name}</a>
+                        <a 
+                        className="transition hover:text-blue-800 text-black" 
+                        href={link}>{name}</a>
                     </li>  
                 ))}
                 </ul>
             </nav>
+            <div className="flex items-center space-x-2 ml-auto mr-30">
+                <a href="#signup" className="px-6 py-2 bg-blue-900 hover:bg-blue-950 transition text-white rounded-md">Sign Up</a>
+                <a href="#login" className="px-6 py-2 border text-black hover:bg-blue-900 hover:text-white transition rounded-md">Log In</a>
+            </div>
         </header>
     )
 }
